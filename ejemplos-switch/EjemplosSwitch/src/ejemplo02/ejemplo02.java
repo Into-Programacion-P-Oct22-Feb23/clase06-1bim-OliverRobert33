7935333d668e5aa52aa75eab1fa87f537b8875c6
Oliver Saraguro <OliverSaraguro@gmail.com>
2022-11-16 10:54:17 -0500

    subiendo clase06

---
+++ ejemplos-switch/EjemplosSwitch/src/ejemplo02/ejemplo02.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo02;
+
+/**
+ *
+ * @author SALA I
+ */
+public class ejemplo02 {
+
+    public static void main(String[] args) {
+        String pais;
+
+        pais = "España";
+        char letra = pais.charAt(0);
+        System.out.printf("%s\n", pais);
+    }
+
+}
